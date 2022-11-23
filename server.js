e15aef2e5414fcea47d5b7677434666ce83c82e5
chirumer <imchirumer@gmail.com>
2022-11-23 09:19:07 +0530

    show different view for pre & post event

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const express = require("express")
+const express = require('express')
 const path = require('path')
+
+const settings = require('./site_settings.json')
+const event_start = new Date(settings.event_start)
+const event_end = new Date(settings.event_end)
 
 const app = express();
 
@@ -11,10 +15,17 @@ app.get("/", (req, res) => {
   res.redirect('/home')
 });
 
+if (Date.now() < event_start){
+  app.use(express.static('pre-event_public'));
+}
+else if (Date.now() > event_end) {
+  app.use(express.static('post-event_public'));
+}
+else {
+  app.use(express.static('public'));
+}
 
-app.use(express.static('public'))
 
-
-app.listen(3000, () => {
-  console.log('Listening on port ' + 3000);
+app.listen(settings.PORT, () => {
+  console.log('Listening on port ' + settings.PORT);
 });
