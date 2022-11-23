99a830cf9d838d3860cd8eeb75a9e6800b3d8d9e
chirumer <imchirumer@gmail.com>
2022-11-23 09:44:11 +0530

    dynamically associate public directory

--- server.js
+++ server.js
@@ -9,24 +9,62 @@ const app = express();
 
 
 
-if (Date.now() < event_start){
-  app.use(express.static('pre-event_public'));
-  app.get("/", (req, res) => {
-    res.redirect('/home')
-  });
-}
-else if (Date.now() > event_end) {
-  app.use(express.static('post-event_public'));
-  app.get("/", (req, res) => {
-    res.redirect('/leaderboard')
-  });
-}
-else {
-  app.use(express.static('public'));
-  app.get("/", (req, res) => {
-    res.redirect('/home')
-  });
-}
+
+app.use ((req, res, next) => {
+
+  const pre_event_pub = 'pre-event_public'
+  const event_pub = 'public'
+  const post_event_pub = 'post-event_public'
+
+  const time_now = Date.now();
+
+  let public_dir;
+
+  if (time_now < event_start) {
+    public_dir = pre_event_pub;
+    if (req.url == '/') {
+      res.redirect('/home')
+    }
+  }
+  else if (time_now > event_end) {
+    public_dir = post_event_pub;
+    if (req.url == '/') {
+      res.redirect('/leaderboard')
+    }
+  }
+  else {
+    public_dir = event_pub;
+    if (req.url == '/') {
+      res.redirect('/home')
+    }
+  }
+
+  const middleware = express.static(path.join (__dirname, public_dir));
+  middleware(req, res, next);
+});
+
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(public, 'index.html'));
+// });
+
+// if (Date.now() < event_start){
+//   app.use(express.static('pre-event_public'));
+//   app.get("/", (req, res) => {
+//     res.redirect('/home')
+//   });
+// }
+// else if (Date.now() > event_end) {
+//   app.use(express.static('post-event_public'));
+//   app.get("/", (req, res) => {
+//     res.redirect('/leaderboard')
+//   });
+// }
+// else {
+//   app.use(express.static('public'));
+//   app.get("/", (req, res) => {
+//     res.redirect('/home')
+//   });
+// }
 
 
 app.listen(settings.PORT, () => {
